cc0e5435606c9ab51e89ae58136ecc502ae2cd4a
marietawele <marietawele700@gmail.com>
2024-01-30 15:47:24 +0000

    kjhg

--- informations.php
+++ informations.php
@@ -1,16 +1,19 @@
 <?php
 
 use Taf\TafAuth;
+use Taf\TableDocumentation;
+
 
 try {
     require './TafConfig.php';
-    $taf_config = new \Taf\TafConfig();  
+    require './TableDocumentation.php';
+    $taf_config = new \Taf\TafConfig();
     $taf_config->allow_cors();
     $tables = $taf_config->tables;
     $reponse = array();
     foreach ($tables as $table_name) {
         $file_count = 0;
-        $dir    = './';
+        $dir = './';
         $files = scandir($dir);
         foreach ($files as $file) {
             if (array_search($table_name, $files)) {
@@ -25,6 +28,15 @@ try {
     $reponse["nom_base_de_donnees"] = $taf_config->database_name;
     $reponse["connexion"] = $taf_config->is_connected();
     $reponse["username"] = $taf_config->user;
+    $reponse["table_v1"] = array_map(function ($une_table) {
+        $docs = new TableDocumentation($une_table);
+        return ["table" => $une_table, "description" => $docs->description, "les_types" => ["add", "edit", "list", "details"]];
+    }, $taf_config->tables);
+    $reponse["table_v2"] = array_map(function ($une_table) {
+        $docs = new TableDocumentation($une_table);
+        return ["table" => $une_table, "description" => $docs->description, "table_descriptions" => $docs->table_descriptions, "les_types" => ["add", "edit", "list", "details"]];
+    }, $taf_config->tables);
+    $reponse["base_url"] = $taf_config->get_base_url();
     $reponse["status"] = true;
     echo json_encode($reponse);
 } catch (\Throwable $th) {
